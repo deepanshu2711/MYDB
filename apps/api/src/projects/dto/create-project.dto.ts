189a39c38087b218a09c7031b8b1eecda7d06a80
Deepanshu <deepanshusaini2711@gmail.com>
2026-04-23 11:46:03 +0530

    ádd decorators in create project dto

--- apps/api/src/projects/dto/create-project.dto.ts
+++ apps/api/src/projects/dto/create-project.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
 export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/)
   password: string;
 }
